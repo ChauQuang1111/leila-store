0abed7778c27621e114b508fa97d74d735d2bcf6
Ducky <chauquang552000@gmail.com>
2024-05-20 14:44:32 +0700

    first commit
    homepage

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import Collections from "@/components/Collections";
+import Navbar from "@/components/Navbar";
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
+export default function Home(){
+  return(
+    <>
+    <Image src="/banner.jpg" alt="banner" width={2000} height={500} className="w-screen" />
+    <Collections />
+    <ProductList />
+    </>
+);
+}
